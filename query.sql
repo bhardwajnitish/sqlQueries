f8db06ea276b60687d609c2b9273c5a44d0f5493
Nitish Bhardwaj <nitish.java@gmail.com>
2019-09-02 23:07:35 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,13 @@
-
+Find Nth highest salary
+  Common SQL
+    select *  from Employee e where (N-1) = (select count(distinct e1.salary) from Employee e1 where e1.salary > e.salary);
+    N -> nth highest salary
+    
+  MySQL
+    select * from employee order by salary desc limit N-1, 1;
+    N -> nth highest salary
+    
+    
+Find duplicate record
+  
+   
